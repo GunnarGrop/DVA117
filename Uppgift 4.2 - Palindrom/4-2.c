66cc6b4712ab9bb3a18138d500c2a6926e008fea
Philip Andersson <philip.e.g.andersson@protonmail.com>
2018-09-18 00:08:23 +0200

    Adding uppgift 4.2

--- Uppgift 4.2 - Palindrom/4-2.c
+++ Uppgift 4.2 - Palindrom/4-2.c
@@ -1,29 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX 60
 
+/**************************
+* Uppgift 4.2             *
+* DVA117 - Programmering  *
+* Av Philip Andersson     *
+**************************/
+
 int isPalindrom(char inputString[]);
+void excludeNonAlpha(char inputString[]);
+void convertToLowercase(char inputString[]);
 
 int main(void) {
-    int checkReturn;
-    char input[MAX];
+    int checkReturn, running = 1;
+    char input[MAX], keepRunning;
 
-    printf("Kolla om en sträng är ett palindrom.\n");
-    printf("Skriv in ett ord eller mening: ");
-    gets(input);
+    while (running == 1) {
+        printf("Kolla om en sträng är ett palindrom.\n");
+        printf("Skriv in ett ord eller mening: \n");
+        while (getchar() != '\n');
+        gets(input);
 
-    checkReturn = isPalindrom(input);
-    if(checkReturn == 1) {
-        printf("\nSträngnen är ett palindrom!\n");
+        excludeNonAlpha(input);
+        convertToLowercase(input);
+
+        checkReturn = isPalindrom(input);
+        if(checkReturn == 1) {
+            printf("\nSträngnen är ett palindrom!\n");
+        }
+        else {
+            printf("\nSträngen är inte ett palindrom.\n");
+        }
+
+
+        do {
+            printf("\nVill du köra programet igen? (j/n)\n");
+            scanf("%c", &keepRunning);
+        } while(keepRunning != 'j' && keepRunning != 'n');
+
+        if(keepRunning == 'n') {
+            running = 0;
+        }
+        else if(keepRunning == 'j') {
+            system("clear");
+        }
     }
-    else {
-        printf("\nSträngen är inte ett palindrom.\n");
-    }
 
+    printf("Färdigt\n");
     return 0;
 }
 
+//Kollar om en sträng är ett palindrom
 int isPalindrom(char inputString[]) {
     int len = strlen(inputString), isTheSame = 1;
     char checkInput[MAX];
@@ -39,4 +70,35 @@ int isPalindrom(char inputString[]) {
     }
 
     return isTheSame;
+}
+
+//Exluderar icke-alfabetiska tecken i inputString
+void excludeNonAlpha(char inputString[]) {
+    int position = 0, len = strlen(inputString);
+    char alphaString[MAX];
+
+    for(int i = 0; i < len; i++) {
+        if(isalpha(inputString[i]) != 0) {
+            alphaString[position] = inputString[i];
+            position++;
+        }
+    }
+
+    for(int i = 0; i < len; i++) {
+        inputString[i] = alphaString[i];
+    }
+}
+
+//Omvandlar alla versaler i inputString[] till gemener
+void convertToLowercase(char inputString[]) {
+    int len = strlen(inputString);
+    char lowercaseString[MAX];
+
+    for(int i = 0; i < len; i++) {
+        lowercaseString[i] = tolower(inputString[i]);
+    }
+
+    for(int i = 0; i < len; i++) {
+        inputString[i] = lowercaseString[i];
+    }
 }
