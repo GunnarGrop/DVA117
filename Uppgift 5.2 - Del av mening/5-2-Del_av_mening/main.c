ec0a0548d6899efa68a54ca875bbc4752d50b1b7
Philip Andersson <philip.e.g.andersson@protonmail.com>
2018-10-08 19:08:09 +0200

    Nonsence commit

---
+++ Uppgift 5.2 - Del av mening/5-2-Del_av_mening/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+#define MAX 100
+
+int main() {
+    char input[MAX], *wordPointer, running;
+
+    do {
+        system("clear");
+        printf("Skriv in en mening: \n");
+        fgets(input, MAX, stdin);
+
+        int wordCount = 1;
+        for (int i = 1; i < strlen(input); i++) {
+            if(!(isspace(input[i])) && isspace(input[i - 1])){
+                wordCount++;
+            }
+            if (wordCount == 3) {
+                wordPointer = &input[i];
+                break;
+            }
+        }
+
+        if (wordCount < 3) {
+            printf("Du har skrivit in för få ord.\n");
+        }
+        else {
+            fputs(wordPointer, stdout);
+        }
+
+        do {
+            printf("\nVill du köra programmet igen? (j/n): ");
+            scanf("%c", &running);
+            while ((getchar()) != '\n');
+        }while(running != 'j' && running != 'n');
+
+    }while(running == 'j');
+
+    return 0;
+}
---
+++ Uppgift 5.3 - Frekvensanalys/5-3-Frekvensanalys/main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define MAX 100
+#define MAXNUMBER 1000
+
+void lookForMostFrequent(int inputArray[MAX], int *number,int *frequency);
+
+int main(){
+    int input[MAX], mostFrequentNumber, frequencyAmount;
+
+    printf("Vänligen mata in tal mellan 0-1000 (max 100st). Avsluta med ett negativt tal.\n");
+
+    for(int i = 0, tmp = 0; i < MAX && tmp >= 0; i++){
+        scanf("%d", &tmp);
+        input[i] = tmp;
+    }
+
+    lookForMostFrequent(input, &mostFrequentNumber, &frequencyAmount);
+    printf("\nTalet %d förekommer flest gånger, totalt %d gånger.\n", mostFrequentNumber, frequencyAmount);
+
+    return 0;
+}
+
+void lookForMostFrequent(int inputArray[MAX], int *number,int *frequency){
+    int tmpFrequency = 0, mostFrequentNumber, frequencyAmount;
+
+    //Går igenom alla tal, 0 till MAXNUMBER
+    for(int i = 0; i <= MAXNUMBER; i++){
+        tmpFrequency = 0;
+
+        /*Går igenom alla positioner i inputArray och kollar om
+        ett tal förkommer fler gånger*/
+        for(int j = 0; j < MAX && inputArray[j] >= 0; j++){
+            if(inputArray[j] == i){
+                tmpFrequency++;
+            }
+        }
+
+        if(tmpFrequency > frequencyAmount){
+            frequencyAmount = tmpFrequency;
+            mostFrequentNumber = i;
+        }
+    }
+    *frequency = frequencyAmount;
+    *number = mostFrequentNumber;
+}
