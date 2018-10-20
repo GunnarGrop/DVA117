9e450aaa6a6b075fead702a6ea40c4ba26f2895e
Philip Andersson <philip.e.g.andersson@protonmail.com>
2018-10-20 18:57:23 +0200

    upd

--- Uppgift 6 - Strukturer/6-Strukturer/main.c
+++ Uppgift 6 - Strukturer/6-Strukturer/main.c
@@ -1,31 +1,42 @@
 #include <stdio.h>
+#include <string.h>
+#define ITEMSIZE 20
+#define UNITSIZE 10
 
 typedef struct{
-    char item[20];
+    char item[ITEMSIZE];
     float amount;
-    char unit[10];
+    char unit[UNITSIZE];
+}groceryItem;
+
+typedef struct{
+    groceryItem ShoppingListItem[5];
+    int itemsInList;
 }shoppingList;
 
 void editList(shoppingList *pGroceries);
 void printList(shoppingList *pGroceries);
-int quit(int running);
+int checkInputForErrors(float input, int scanfOutput);
 
 int main()
 {
     int choice;
     _Bool running = 1;
-
     shoppingList groceries[5];
 
-    printf("1. Edit list.\n2. Print list.\n3. Quit.\nVad vill du göra? ");
-
-    int listNumber = 0;
+    groceries->itemsInList = 0;
     while(running){
+        printf("1. Edit list.\n2. Print list.\n3. Quit.\nVad vill du göra? ");
         scanf("%d", &choice);
+
         switch(choice){
         case 1:
-            editList(&groceries[listNumber]);
-            listNumber++;
+            if(groceries->itemsInList <= 4){
+                editList(&groceries[groceries->itemsInList]);
+                groceries->itemsInList++;
+            }else{
+                printf("\nYou cannot add more items to your shopping list.\n\n");
+            }
             break;
         case 2:
             printList(groceries);
@@ -36,28 +47,47 @@ int main()
             printf("Please choose one.");
         }
     }
-
     return 0;
 }
 
 void editList(shoppingList *pGroceries)
 {
-    printf("\nItem: ");
-    scanf("%s", pGroceries->item);
+    system("clear");
+    printf("Item: ");
+    while ( getchar() != '\n' );
+    gets(pGroceries->ShoppingListItem->item);
 
-    printf("Amount: ");
-    scanf("%.2f", pGroceries->amount);
+    int scanfOutput, errorCheck;
+    float tmpInput;
+    do{
+        printf("Amount: ");
+        scanfOutput = scanf("%f", &tmpInput);
+        while ( getchar() != '\n' );
+        errorCheck = checkInputForErrors(tmpInput, scanfOutput);
+    }while(errorCheck != 0);
+    pGroceries->ShoppingListItem->amount = tmpInput;
 
     printf("Unit: ");
-    scanf("%s", pGroceries->unit);
+    //while ( getchar() != '\n' );
+    gets(pGroceries->ShoppingListItem->unit);
+    printf("\n");
+    system("clear");
 }
 
 void printList(shoppingList *pGroceries)
 {
-    for(int i = 0; i < 5; i++){
-        printf("%s", *(pGroceries+i)->item);
-        printf("%s", pGroceries[i].item);
-        //printf("%f", *(pGroceries+i)->amount);
-        printf("%s", *(pGroceries+i)->unit);
+    system("clear");
+    printf("You have %d items in you list:\n", pGroceries->itemsInList);
+    for(int i = 0; i < pGroceries->itemsInList; i++){
+        printf("%-15s\t%.1f%s\n",pGroceries[i].ShoppingListItem->item, pGroceries[i].ShoppingListItem->amount, pGroceries[i].ShoppingListItem->unit);
+    }
+    printf("\n");
+}
+
+int checkInputForErrors(float input, int scanfOutput){
+    if(input < 0 || scanfOutput == 0){
+        return 1;
+    }else{
+        return 0;
     }
 }
