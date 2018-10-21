7d3060f4cce75aa0e7d2e37cfae0f3535db4b5d3
Philip Andersson <philip.e.g.andersson@protonmail.com>
2018-10-21 23:18:31 +0200

    cleaning code

--- Uppgift 6 - Strukturer/6-Strukturer/main.c
+++ Uppgift 6 - Strukturer/6-Strukturer/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define ITEMSIZE 20
 #define UNITSIZE 10
 
@@ -14,80 +15,92 @@ typedef struct{
     int itemsInList;
 }shoppingList;
 
-void editList(shoppingList *pGroceries);
+void editList(groceryItem *pGroceries);
 void printList(shoppingList *pGroceries);
-int checkInputForErrors(float input, int scanfOutput);
+int checkInputForErrors(float input, char catch, int scanfOutput);
+float tryInput(char output[], char failedOutput[]);
 
-int main()
-{
-    int choice;
+int main(){
     _Bool running = 1;
-    shoppingList groceries[5];
+    shoppingList groceries;
+    groceries.itemsInList = 0;
 
-    groceries->itemsInList = 0;
     while(running){
-        printf("1. Edit list.\n2. Print list.\n3. Quit.\nVad vill du göra? ");
-        scanf("%d", &choice);
+        int choice = tryInput("1. Edit list.\n2. Print list.\n3. Quit.\nChoice: ", "\nPlease choose one.\n\n");
 
         switch(choice){
         case 1:
-            if(groceries->itemsInList <= 4){
-                editList(&groceries[groceries->itemsInList]);
-                groceries->itemsInList++;
+            if(groceries.itemsInList <= 4){
+                editList(&groceries.ShoppingListItem[groceries.itemsInList]);
+                groceries.itemsInList++;
             }else{
                 printf("\nYou cannot add more items to your shopping list.\n\n");
             }
             break;
         case 2:
-            printList(groceries);
+            printList(&groceries);
             break;
         case 3:
             running = 0;
+            break;
         default:
-            printf("Please choose one.");
+            printf("\nPlease choose one.\n\n");
         }
     }
+
     return 0;
 }
 
-void editList(shoppingList *pGroceries)
-{
+void editList(groceryItem *pGroceries){
     system("clear");
     printf("Item: ");
-    while ( getchar() != '\n' );
-    gets(pGroceries->ShoppingListItem->item);
+    gets(pGroceries->item);
 
-    int scanfOutput, errorCheck;
-    float tmpInput;
-    do{
-        printf("Amount: ");
-        scanfOutput = scanf("%f", &tmpInput);
-        while ( getchar() != '\n' );
-        errorCheck = checkInputForErrors(tmpInput, scanfOutput);
-    }while(errorCheck != 0);
-    pGroceries->ShoppingListItem->amount = tmpInput;
+    float tmpInput = tryInput("Amount: ", "Invalid input. Please try again.\n");
+    pGroceries->amount = tmpInput;
 
     printf("Unit: ");
-    //while ( getchar() != '\n' );
-    gets(pGroceries->ShoppingListItem->unit);
+    gets(pGroceries->unit);
     printf("\n");
     system("clear");
 }
 
-void printList(shoppingList *pGroceries)
-{
+void printList(shoppingList *pGroceries){
     system("clear");
     printf("You have %d items in you list:\n", pGroceries->itemsInList);
     for(int i = 0; i < pGroceries->itemsInList; i++){
-        printf("%-15s\t%.1f%s\n",pGroceries[i].ShoppingListItem->item, pGroceries[i].ShoppingListItem->amount, pGroceries[i].ShoppingListItem->unit);
+        printf("%-15s\t%.1f%s\n",pGroceries->ShoppingListItem[i].item, pGroceries->ShoppingListItem[i].amount, pGroceries->ShoppingListItem[i].unit);
     }
     printf("\n");
 }
 
-int checkInputForErrors(float input, int scanfOutput){
-    if(input < 0 || scanfOutput == 0){
+/*Kollar om ett siffer-input är korrekt angivet, större än 0 och om det är
+  rätt tecken efter siffran (enterslag)*/
+int checkInputForErrors(float input, char catch, int scanfOutput){
+    if(input < 0 || scanfOutput == 0 || catch != '\n'){
+        if(catch != '\n'){
+            while(getchar() != '\n');
+        }
         return 1;
-    }else{
-        return 0;
     }
+
+    return 0;
+}
+
+//Försök kontinuerligt ta input från användaren
+float tryInput(char output[], char failedOutput[]){
+    int scanfOutput, errorCheck;
+    float choice;
+    char catchNewLine;
+
+    do{
+        printf(output);
+        scanfOutput = scanf("%f%c", &choice, &catchNewLine);
+        errorCheck = checkInputForErrors(choice, catchNewLine, scanfOutput);
+        if(errorCheck != 0){
+            printf(failedOutput);
+        }
+    }while(errorCheck != 0);
+
+    return choice;
 }
